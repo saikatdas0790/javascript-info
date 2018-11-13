7f9868b97a2699cafbc37793c32c68602bb1ff95
Saikat Das <saikatdas0790@gmail.com>
2018-11-13 09:56:08 +0530

    Map to objects

---
+++ part-1-the-javascript-language/5-data-types/5.5-array-methods/8-map-to-objects.test.js
@@ -0,0 +1,20 @@
+describe("Users mapped", () => {
+  let john = { name: "John", surname: "Smith", id: 1 };
+  let pete = { name: "Pete", surname: "Hunt", id: 2 };
+  let mary = { name: "Mary", surname: "Key", id: 3 };
+
+  let users = [john, pete, mary];
+
+  it("maps to objects", () => {
+    let usersMapped = users.map(user => ({
+      fullName: `${user.name} ${user.surname}`,
+      id: user.id
+    }));
+
+    expect(usersMapped).toEqual([
+      { fullName: "John Smith", id: 1 },
+      { fullName: "Pete Hunt", id: 2 },
+      { fullName: "Mary Key", id: 3 }
+    ]);
+  });
+});
