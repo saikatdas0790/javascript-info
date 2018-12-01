ed78d8f728eccbbef6190b89369bdbe5ea1f91c7
Saikat Das <saikatdas0790@gmail.com>
2018-12-01 20:16:52 +0530

    Get average age

---
+++ part-1-the-javascript-language/5-data-types/5.5-array-methods/11-get-average-age.test.js
@@ -0,0 +1,17 @@
+describe("Get average age", () => {
+  let john = { name: "John", age: 25 };
+  let pete = { name: "Pete", age: 30 };
+  let mary = { name: "Mary", age: 29 };
+
+  let arr = [john, pete, mary];
+
+  let getAverageAge = persons => {
+    return (
+      persons.reduce((sum, current) => sum + current.age, 0) / persons.length
+    );
+  };
+
+  test("Average age", () => {
+    expect(getAverageAge(arr)).toBe(28);
+  });
+});
