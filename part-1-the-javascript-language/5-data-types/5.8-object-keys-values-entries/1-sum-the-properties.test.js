8507d16e055a6e30f446051763dad17e36eec1cb
Saikat Das <saikatdas0790@gmail.com>
2018-12-02 14:41:35 +0530

    Sum the properties

---
+++ part-1-the-javascript-language/5-data-types/5.8-object-keys-values-entries/1-sum-the-properties.test.js
@@ -0,0 +1,18 @@
+describe("sumSalaries", () => {
+  const sumSalaries = salaries => {
+    return Object.values(salaries).reduce((sum, current) => sum + current, 0);
+  };
+  it("returns sum of salaries", () => {
+    let salaries = {
+      John: 100,
+      Pete: 300,
+      Mary: 250
+    };
+
+    expect(sumSalaries(salaries)).toBe(650);
+  });
+
+  it("returns 0 for the empty object", () => {
+    expect(sumSalaries({})).toBe(0);
+  });
+});
